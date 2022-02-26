f2bc12d859131ae1facc1628ca14139338c46005
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-26 23:54:45 +0000

    Implement fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 // const callback = function (error, ip) {
 //   if (error) {
@@ -11,3 +11,11 @@
 // fetchMyIP(callback)
 
 
+
+// const callback = function(error, coordinates) {
+//   if (error) {
+//     console.log('It didn\'t work:', error);
+//   }
+//   console.log('It worked! Returned coordinates:', coordinates);
+// };
+// fetchCoordsByIP('199.126.227.229', callback);
--- iss.js
+++ iss.js
@@ -21,4 +21,20 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching Coords for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const { latitude, longitude} = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchCoordsByIP };
