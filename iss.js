f6df9a914854d8cc5f3200c8bdd6bdc32e172a79
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-26 20:00:40 +0000

    Write logic to fetch ip via api endpoint

---
+++ iss.js
@@ -0,0 +1,23 @@
+const request = require("request");
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+
+const fetchMyIP = function (callback) {
+  // use request to fetch IP address from JSON API
+  request(`https://pi.ipify.org?format=json`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    callback(null, data);
+  });
+};
+
+module.exports = { fetchMyIP };
