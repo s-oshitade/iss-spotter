b1996d81c969136cf1d8ebc20c3841d6e3194715
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-26 22:46:50 +0000

    Refactor for readability

--- iss.js
+++ iss.js
@@ -8,20 +8,16 @@ const request = require("request");
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
 
-const fetchMyIP = function (callback) {
-  // use request to fetch IP address from JSON API
+const fetchMyIP = function(callback) {
   request(`https://api.ipify.org?format=json`, (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
-    if(response.statusCode !== 200){
+    if (error) return callback(error, null);
+    if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
       return;
     }
-    const data = JSON.parse(body);
-    callback(null, data);
+    const ip = JSON.parse(body);
+    callback(null, ip);
   });
 };
 
